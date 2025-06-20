4cc970650dcc3de622415230ca723a1f2e59f368
Fábio <fabiomotamarques815@gmail.com>
2025-06-20 14:58:38 -0300

    construindo o menu

--- Pousada/src/br/ifsc/pousada/modelos/Cliente.java
+++ Pousada/src/br/ifsc/pousada/modelos/Cliente.java
@@ -2,4 +2,12 @@ package br.ifsc.pousada.modelos;
 
 public class Cliente extends Pessoa {
 	private String cliente;
+
+	public String getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
 }
